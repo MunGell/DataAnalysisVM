1c1983447910377d7893416a3a5ad66f083ede72
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2016-03-27 14:03:56 +1100

    Update jupyter installation and add notebooks directory

--- setup.sh
+++ setup.sh
@@ -7,8 +7,7 @@ sudo apt-get build-dep -y python-matplotlib
 pip install -r /vagrant/requirements.txt
 sudo unlink /etc/init.d/jupyter
 sudo ln -s /vagrant/init.d/jupyter /etc/init.d/jupyter
-sudo chmod 755 /etc/init.d/jupyter
-sudo update-rc.d jupyter defaults 97 03
-unlink /home/vagrant/.jupyter
+sudo chmod +x /etc/init.d/jupyter
+sudo update-rc.d jupyter defaults
 ln -s /vagrant/.jupyter /home/vagrant/.jupyter
 sudo service jupyter restart
